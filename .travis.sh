c2a948d61553d6c623c1de5cb2e9e7024d6e0bd1
BB9z <bb9z@me.com>
2020-01-19 21:17:56 +0800

    Update RFAPI. #ci config update.

--- .travis.sh
+++ .travis.sh
@@ -14,10 +14,22 @@ logError () {
     echo "\033[31m$1\033[0m" >&2
 }
 
-logInfo "$TRAVIS_COMMIT_MESSAGE"
+# Make sure all parameters are set correctly.
+logInfo "RFCI_PRODUCT_NAME = $RFCI_PRODUCT_NAME"
+
+readonly RFCI_TASK="${RFCI_TASK:? is not set.}"
 logInfo "RFCI_TASK = $RFCI_TASK"
-readonly RFSTAGE="$1"
-logInfo "RFSTAGE = $RFSTAGE"
+
+readonly RFCI_STAGE="${1:?STAGE is not set.}"
+logInfo "RFCI_STAGE = $RFCI_STAGE"
+
+readonly RFWorkspace=${RFWorkspace:="$RFCI_PRODUCT_NAME.xcworkspace"}
+logInfo "RFWorkspace = $RFWorkspace"
+
+TRAVIS_COMMIT_MESSAGE=${TRAVIS_COMMIT_MESSAGE:="$(logWarning 'TRAVIS_COMMIT_MESSAGE is not set, leave it blank.')"}
+TRAVIS_BRANCH=${TRAVIS_BRANCH:="$(logWarning 'TRAVIS_BRANCH is not set, leave it blank.')"}
+
+echo ""
 
 # Run test
 # $1 scheme
@@ -31,9 +43,28 @@ XC_TestMac() {
     xcodebuild test -enableCodeCoverage YES -workspace "$RFWorkspace" -scheme "Test-macOS" GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
 }
 
-# Run watchOS build
+# Run watchOS test
 XC_TestWatch() {
     xcodebuild build -workspace "$RFWorkspace" -scheme "Target-watchOS" ONLY_ACTIVE_ARCH=NO | xcpretty
+}
+
+# Run tests on iOS Simulator.
+# The destinations are the first and last available destination that are automatically detected.
+# $1 scheme
+XC_TestAutoIOS() {
+    logInfo "Detecting destinations..."
+    destList=$(xcodebuild -showdestinations -workspace "$RFWorkspace" -scheme "$1" | grep "iOS Simulator")
+    destCount=$(echo "$destList" | wc -l)
+    destFirst=$(echo "$destList" | head -1)
+    destLast=$(echo "$destList" | tail -2 | head -1)
+    destFirstID=$(echo "$destFirst" | awk 'match($0,/id\:[0-9A-F-]+/){ print substr($0,RSTART+3,RLENGTH-3) }')
+    destLastID=$(echo "$destLast" | awk 'match($0,/id\:[0-9A-F-]+/){ print substr($0,RSTART+3,RLENGTH-3) }')
+
+    logWarning "Test on simulator (id: $destFirstID)."
+    XC_Test "$1" "platform=iOS Simulator,id=$destFirstID"
+
+    logWarning "Test on simulator (id: $destLastID)."
+    XC_Test "$1" "platform=iOS Simulator,id=$destLastID"
 }
 
 STAGE_SETUP() {
@@ -45,7 +76,7 @@ STAGE_MAIN() {
         if [[ "$TRAVIS_COMMIT_MESSAGE" = *"[skip lint]"* ]]; then
             logWarning "Skip pod lint"
         else
-            logInfo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+            logInfo "Lint the podspec."
 
             # Modify podspec, add shadow.m
             # Replace 'MBAppKit/**/*' => 'Test/Shared/shadow.m',\1
@@ -55,17 +86,17 @@ STAGE_MAIN() {
             pod lib lint --fail-fast --allow-warnings
         fi
 
-    elif [ "$RFCI_TASK" = "Xcode9" ]; then
+    elif [ "$RFCI_TASK" = "Xcode10" ]; then
         pod install
-        XC_Test "Test-iOS" "platform=iOS Simulator,name=iPhone X,OS=11.3"
-        XC_Test "Test-iOS" "platform=iOS Simulator,name=iPhone 5,OS=9.0"
+        XC_TestMac
+        XC_TestAutoIOS "Test-iOS"
     else
         logError "Unexpected CI task: $RFCI_TASK"
     fi
 }
 
 STAGE_SUCCESS() {
-    if [ "$RFCI_TASK" = "Xcode9" ]; then
+    if [ "${RFCI_COVERAGE-}" = "1" ]; then
         curl -s https://codecov.io/bash | bash -s
     fi
 }
@@ -76,4 +107,4 @@ STAGE_FAILURE() {
     fi
 }
 
-"STAGE_$RFSTAGE"
+"STAGE_$RFCI_STAGE"
